8f1b77bdda98b8ae9728a72cad15ace6bab28918
vanphutin2004 <vanphutin.contact@gmail.com>
2025-01-14 10:50:36 +0700

    feat : feature more for header

--- client/src/components/header/header-sub/HeaderSub.tsx
+++ client/src/components/header/header-sub/HeaderSub.tsx
@@ -1,12 +1,15 @@
 import React, { useContext } from "react";
 import "./_headerSub.scss";
-import { NavLink } from "react-router-dom";
+import { NavLink, useLocation, useNavigate } from "react-router-dom";
 import { MdArrowDropDown } from "react-icons/md";
 import { MenuContext } from "../../../context/menuToggle/MenuContext";
 import { useTranslation } from "react-i18next";
+import { getMoviesByCategory } from "../../../services/apiMovie";
 
 const HeaderSub = () => {
   const { t } = useTranslation();
+  const location = useLocation(); // Lấy thông tin về URL hiện tại
+  const navigate = useNavigate(); // Dùng để điều hướng
 
   const MENU_SUB_ARRAY_LIST: {
     [key: string]: {
@@ -14,6 +17,7 @@ const HeaderSub = () => {
       link: string;
       subMenu: string[];
       icon?: React.ReactNode;
+      href?: string;
     };
   } = {
     home: { name: t("header.menu.home"), link: "/", subMenu: [] },
@@ -21,35 +25,53 @@ const HeaderSub = () => {
       name: t("header.menu.singleMovies"),
       link: "/single-movies",
       subMenu: [],
+      href: "#single-section",
     },
     TVSeries: {
       name: t("header.menu.tvSeries"),
-      link: "#",
-      subMenu: [
-        t("header.menu.subMenu.action"),
-        t("header.menu.subMenu.comedy"),
-        t("header.menu.subMenu.horror"),
-        t("header.menu.subMenu.romance"),
-        t("header.menu.subMenu.scienceFiction"),
-        t("header.menu.subMenu.adventure"),
-        t("header.menu.subMenu.animation"),
-        t("header.menu.subMenu.war"),
-        t("header.menu.subMenu.drama"),
-        t("header.menu.subMenu.crime"),
-        t("header.menu.subMenu.musical"),
-        t("header.menu.subMenu.documentary"),
-        t("header.menu.subMenu.fantasy"),
-        t("header.menu.subMenu.family"),
-      ],
-      icon: <MdArrowDropDown />,
+      link: "/tv-series",
+      subMenu: [],
+      href: "#tvseries-section",
+    },
+    TVShows: {
+      name: t("header.menu.tvshows"),
+      link: "/tv-shows",
+      subMenu: [],
+      href: "#tvshows-section",
     },
     movieGenres: {
       name: t("header.menu.movieGenres"),
-      link: "/movie-genres",
+      link: "#",
+      subMenu: [
+        "hanh-dong",
+        "bi-an",
+        "hai-huoc",
+        "tinh-cam",
+        "vien-tuong",
+        "khoa-hoc",
+        "tre-em",
+        "chinh-kich",
+        "co-trang",
+        "hinh-su",
+        "vo-thuat",
+        "gia-dinh",
+        "am-nhac",
+        "kinh-di",
+      ],
+      href: "",
+      icon: <MdArrowDropDown />,
+    },
+    blog: {
+      name: t("header.menu.blog"),
+      link: "/blog",
+      subMenu: [],
+      href: "#blog-section",
+    },
+    about: {
+      name: t("header.menu.about"),
+      link: "/about",
       subMenu: [],
     },
-    blog: { name: t("header.menu.blog"), link: "/blog", subMenu: [] },
-    about: { name: t("header.menu.about"), link: "/about", subMenu: [] },
   };
 
   const context = useContext(MenuContext);
@@ -63,18 +85,44 @@ const HeaderSub = () => {
       toggleMenu();
     }
   };
+
+  const handleLinkClick = (href: string) => {
+    // Kiểm tra xem hiện tại có đang ở trang Home hay không
+    if (location.pathname !== "/") {
+      // Nếu không phải trang Home, điều hướng về Home
+      navigate("/", { replace: true });
+      // Sau khi điều hướng về Home, thêm phần hash vào URL
+      setTimeout(() => {
+        window.location.hash = href; // Sau khi vào trang Home, cuộn đến phần mục tiêu
+      }, 0);
+    } else {
+      // Nếu đang ở trang Home, chỉ cần cuộn đến phần mục tiêu
+      window.location.hash = href;
+    }
+  };
+  const handleGetMoviesByCategory = async (item: string) => {
+    console.log("sulg", item);
+    const res = await getMoviesByCategory(item);
+    console.log(res);
+  };
   return (
-    <div className="header header-sub ">
+    <div className="header header-sub">
       <div className="container menu gap-3">
         {Object.entries(MENU_SUB_ARRAY_LIST).map(
-          ([key, { name, link, subMenu, icon }]) => (
+          ([key, { name, link, subMenu, icon, href }]) => (
             <div
               key={key}
-              className="menu-item "
+              className="menu-item"
               data-name={name}
               onClick={handleClick}
             >
-              <NavTitle link={link} name={name} submenu={subMenu} icon={icon} />
+              <NavTitle
+                link={href || link}
+                name={name}
+                submenu={subMenu}
+                icon={icon}
+                onClick={() => href && handleLinkClick(href)} // Gọi handleLinkClick khi nhấn vào
+              />
               {subMenu.length > 0 && (
                 <div className="dropdown-menu">
                   <ul className="submenu">
@@ -83,14 +131,10 @@ const HeaderSub = () => {
                         key={index + item}
                         className="submenu-item"
                         data-name={name}
+                        onClick={() => handleGetMoviesByCategory(item)}
                       >
-                        <NavLink
-                          key={index}
-                          to={`tv-series/${item
-                            .toLowerCase()
-                            .replace(" ", "-")}`}
-                        >
-                          {item}
+                        <NavLink key={index} to={`movie-genres/${item}`}>
+                          {t(`header.menu.subMenu.${item}`)}
                         </NavLink>
                       </li>
                     ))}
@@ -110,12 +154,14 @@ interface NavTitleProps {
   name: string;
   submenu: string[];
   icon: React.ReactNode;
+  onClick: () => void; // Thêm sự kiện onClick
 }
 
-const NavTitle: React.FC<NavTitleProps> = ({ link, name, icon }) => {
+const NavTitle: React.FC<NavTitleProps> = ({ link, name, icon, onClick }) => {
   return (
     <NavLink
       to={link}
+      onClick={onClick}
       className={({ isActive }) => (isActive ? " nav-item" : "nav-item")}
     >
       {name} <span>{icon}</span>
--- client/src/services/apiMovie.ts
+++ client/src/services/apiMovie.ts
@@ -1,5 +1,5 @@
 import { ApiResponseMovieDetail } from "../interfaces/MoiveDetail";
-import { ApiResponseMovie } from "../interfaces/Movie";
+import { ApiResponseMovie, Item } from "../interfaces/Movie";
 import { api2Instance } from "../utils/axiosConfig";
 import { handleApiError } from "../utils/handleApiError";
 import { AxiosResponse } from "axios";
@@ -31,5 +31,39 @@ export const getMovies = async (
     return response.data;
   } catch (error) {
     throw new Error(handleApiError(error));
+  }
+};
+
+//Lấy danh sách phim từ API và lọc theo thể loại
+export const getMoviesByCategory = async (
+  category: string
+): Promise<Item[] | null> => {
+  try {
+    const urls = [
+      `${MOVIE_API}/phim-le`,
+      `${MOVIE_API}/phim-bo`,
+      `${MOVIE_API}/hoat-hinh`,
+      `${MOVIE_API}/tv-shows`,
+    ];
+
+    let allMovies: Item[] = []; // Mảng lưu tất cả các phim
+
+    for (const url of urls) {
+      const response: AxiosResponse = await api2Instance.get(url); // Lấy dữ liệu từ API
+      const movies: Item[] = response.data.items; // Giả sử `items` là mảng chứa các phim
+
+      // Lọc phim theo thể loại
+      const filteredMovies = movies.filter((movie: Item) =>
+        movie.category.some((cat) => cat.slug === category)
+      );
+
+      // Thêm phim đã lọc vào mảng `allMovies`
+      allMovies = allMovies.concat(filteredMovies);
+    }
+
+    return allMovies.length > 0 ? allMovies : null;
+  } catch (error) {
+    console.error("Error fetching movies:", error);
+    throw new Error("Failed to fetch movie data");
   }
 };
